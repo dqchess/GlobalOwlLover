e3c2ba0f5f4e8cf33c537a6498382249c64193b3
Dadathor <nicolas.systermans@outlook.fr>
2020-02-01 22:17:35 +0100

    some fx

--- Assets/Scripts/BombThrower.cs
+++ Assets/Scripts/BombThrower.cs
@@ -10,20 +10,41 @@ public class BombThrower : MonoBehaviour
     public Hud hud; 
     private Animator anim;
 
+    public GameObject eyeLasers;
+    public GameObject bigLaser;
+
+    public float altitude;
+    public float palier1 = 150f;
+    public float palier2 = 250f;
+
+    float timerBomb = 0f;
+    float timerEyeLaser = 0f;
+    float timerBigLaser = 0f;
+
+    public float cooldownBomb = 3f;
+    public float cooldownEyeLaser = 3f;
+    public float cooldownBigLaser = 7f;
+    
+
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        altitude = transform.position.y;
+        timerBomb += Time.deltaTime;
+        timerEyeLaser += Time.deltaTime;
+        timerBigLaser += Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && CanShoot())
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(hud.mousePos.position);
 
             if (Physics.Raycast(ray, out hit))
             {
-                SpawnBomb(hit.point);
+                Shoot(hit.point);
                 anim.SetTrigger("Shoot");
             }
         }
@@ -33,5 +54,68 @@ public class BombThrower : MonoBehaviour
     {
         Instantiate(bomb, transform.position,Quaternion.identity,null);
         bomb.GetComponent<Bomb>().target = p;
+    }
+    private void SpawnEyeLaser(Vector3 p)
+    {
+        Instantiate(bomb, transform.position, Quaternion.identity, null);
+        bomb.GetComponent<Bomb>().target = p;
+    }
+    private void SpawnBigLaser(Vector3 p)
+    {
+        Instantiate(bomb, transform.position, Quaternion.identity, null);
+        bomb.GetComponent<Bomb>().target = p;
+    }
+    private void Shoot(Vector3 p)
+    {
+       if(altitude<palier1)
+        {
+            //Shoot big laser
+            Instantiate(bomb, transform.position, Quaternion.identity, null);
+            bomb.GetComponent<Bomb>().target = p;
+            timerBigLaser = 0;
+            Debug.Log("BigLaser");
+        }
+       else if(altitude<palier2)
+        {
+            //Shoot eye laser
+            Instantiate(bomb, transform.position, Quaternion.identity, null);
+            bomb.GetComponent<Bomb>().target = p;
+            timerEyeLaser = 0;
+            Debug.Log("EyeLaser");
+        }
+       else
+        {
+            //Shoot bomb
+            Instantiate(bomb, transform.position, Quaternion.identity, null);
+            bomb.GetComponent<Bomb>().target = p;
+            timerBomb = 0;
+            Debug.Log("Bomb");
+        }
+    }
+    bool CanShoot()
+    {
+        bool output;
+        if (altitude < palier1)
+        {
+            if (timerBigLaser > cooldownBigLaser)
+                output = true;
+            else
+                output = false;
+        }
+        else if (altitude < palier2)
+        {
+            if (timerEyeLaser > cooldownEyeLaser)
+                output = true;
+            else
+                output = false;
+        }
+        else
+        {
+            if (timerBomb > cooldownBomb)
+                output = true;
+            else
+                output = false;
+        }
+        return output;
     }
 }
