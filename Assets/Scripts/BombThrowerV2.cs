d02bc524b5c421666c82c74d3283347172ecfc31
Keroro91 <hugo.tubiana@gmail.com>
2020-02-02 12:01:26 +0100

    v*Time.deltaTime*rotateSpeed

---
+++ Assets/Scripts/BombThrowerV2.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MFlight.Demo;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BombThrowerV2 : MonoBehaviour
+{
+    public GameObject bomb;
+    public GameObject bombSpawner;
+    private Animator anim;
+    public GameObject eyeLasers;
+    public GameObject bigLaser;
+
+    public Transform eyeL;
+    public Transform eyeR;
+
+    public float altitude;
+    public float palier1 = 150f;
+    public float palier2 = 250f;
+    public float palierMax = 500f;
+
+    public int indexWeapon = 0;
+
+    float timerBomb = 0f;
+    float timerEyeLaser = 0f;
+    float timerBigLaser = 0f;
+
+    public float cooldownBomb = 1f;
+    public float cooldownEyeLaser = 3f;
+    public float cooldownBigLaser = 7f;
+
+    public Text textBomb;
+    public Text textEyeLaser;
+    public Text textBigLaser;
+
+    private GameObject bl;
+
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+    }
+    void Update()
+    {
+        UpdateUI();
+        altitude = transform.position.y;
+        timerBomb += Time.deltaTime;
+        timerEyeLaser += Time.deltaTime;
+        timerBigLaser += Time.deltaTime;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            indexWeapon++;
+            if (indexWeapon > 2)
+                indexWeapon = 0;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            indexWeapon--;
+            if (indexWeapon < 0)
+                indexWeapon = 2;
+        }
+
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Shoot(hit.point);
+                anim.SetTrigger("Shoot");
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            if (bl != null)
+                Destroy(bl);
+        }
+    }
+
+    public void UpdateUI()
+    {
+        Color cLessAlpha = new Color(255, 255, 255, 0.5f);
+        Color c = new Color(255, 255, 255, 255);
+        switch (indexWeapon)
+        {
+            case (0):
+                textBomb.transform.DOScale(Vector3.one * 1.2f, 0.3f);
+                textBomb.DOColor(c, 0f);
+                textEyeLaser.transform.DOScale(Vector3.one, 0.3f);
+                textEyeLaser.DOColor(cLessAlpha, 0f);
+                textBigLaser.transform.DOScale(Vector3.one, 0.3f);
+                textBigLaser.DOColor(cLessAlpha, 0f);
+                break;
+            case (1):
+                textBomb.transform.DOScale(Vector3.one, 0.3f);
+                textBomb.DOColor(cLessAlpha, 0f);
+                textEyeLaser.transform.DOScale(Vector3.one * 1.2f, 0.3f);
+                textEyeLaser.DOColor(c, 0f);
+                textBigLaser.transform.DOScale(Vector3.one, 0.3f);
+                textBigLaser.DOColor(cLessAlpha, 0f);
+                break;
+            case (2):
+                textBomb.transform.DOScale(Vector3.one, 0.3f);
+                textBomb.DOColor(cLessAlpha, 0f);
+                textEyeLaser.transform.DOScale(Vector3.one, 0.3f);
+                textEyeLaser.DOColor(cLessAlpha, 0f);
+                textBigLaser.transform.DOScale(Vector3.one * 1.2f, 0.3f);
+                textBigLaser.DOColor(c, 0f);
+                break;
+        }
+    }
+
+    private void Shoot(Vector3 p)
+    {
+        Camera.main.transform.parent.DOShakePosition(5f, 0.5f, 5, 90);
+        if (indexWeapon == 2) //big laser
+       {
+            bl = Instantiate(bigLaser, Vector3.zero, Quaternion.identity, null);
+            bl.GetComponent<BigLaser>().owltransform = bombSpawner;
+            bl.GetComponent<BigLaser>().owl = gameObject;
+            timerBigLaser = 0;           
+       }
+       else if(indexWeapon == 1) //eye laser
+       {
+            StartCoroutine(EyeLaserSalve(p));
+            
+            timerEyeLaser = 0;
+       }
+       else if(indexWeapon == 0) //bomb
+       {
+            Instantiate(bomb, transform.position, Quaternion.identity, null);
+            bomb.GetComponent<Bomb>().target = p;
+            timerBomb = 0;
+        }
+    }
+
+    IEnumerator EyeLaserSalve(Vector3 p)
+    {
+        Instantiate(eyeLasers, eyeL.position, Quaternion.identity, transform);
+        Instantiate(eyeLasers, eyeR.position, Quaternion.identity, transform);
+        eyeLasers.GetComponent<EyeLaser>().target = p;
+
+        yield return new WaitForSeconds(0.1f);
+
+        Instantiate(eyeLasers, eyeL.position, Quaternion.identity, transform);
+        Instantiate(eyeLasers, eyeR.position, Quaternion.identity, transform);
+        eyeLasers.GetComponent<EyeLaser>().target = p;
+
+        yield return new WaitForSeconds(0.1f);
+
+        Instantiate(eyeLasers, eyeL.position, Quaternion.identity, transform);
+        Instantiate(eyeLasers, eyeR.position, Quaternion.identity, transform);
+        eyeLasers.GetComponent<EyeLaser>().target = p;
+    }
+    
+    bool CanShoot()
+    {
+        bool output;
+        if (altitude < palier1)
+        {
+            if (timerBigLaser > cooldownBigLaser)
+                output = true;
+            else
+                output = false;
+        }
+        else if (altitude < palier2)
+        {
+            if (timerEyeLaser > cooldownEyeLaser)
+                output = true;
+            else
+                output = false;
+        }
+        else
+        {
+            if (timerBomb > cooldownBomb)
+                output = true;
+            else
+                output = false;
+        }
+        return output;
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
+        
+        Gizmos.DrawRay(Camera.main.ScreenToWorldPoint(Input.mousePosition), direction);
+    }
+}
---
+++ Assets/Scripts/ControllerV2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerV2 : MonoBehaviour
+{
+    public float startSpeed,maxSpeed, rotateSpeed, transitionXSpeed,transitionYSpeed;
+    private float speed;
+
+    public GameObject Pivot;
+
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed =startSpeed;
+        offset = transform.position-Camera.main.transform.position;
+
+
+        
+    }
+
+  
+ float ClampAngle(float angle, float from, float to)
+     {
+         // accepts e.g. -80, 80
+         if (angle < 0f) angle = 360 + angle;
+         if (angle > 180f) return Mathf.Max(angle, 360+from);
+         return Mathf.Min(angle, to);
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h, v;
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        transform.position += transform.forward*speed*Time.deltaTime;
+
+        
+        Pivot.transform.position = transform.position;
+
+        Debug.Log(transform.eulerAngles.x);
+
+    
+            Vector3 rot = transform.rotation.eulerAngles + new Vector3(v*Time.deltaTime*rotateSpeed, h*Time.deltaTime*rotateSpeed, 0f); //use local if your char is not always oriented Vector3.up
+            rot.x = ClampAngle(rot.x, -60f, 90f);
+            rot.y = ClampAngle(rot.y, -90f, 90f);
+         
+            
+            
+           
+            Pivot.transform.rotation =  Quaternion.Lerp(Pivot.transform.rotation, transform.rotation, Time.deltaTime / transitionXSpeed);
+
+            
+           
+                   
+
+            
+           
+        
+        
+
+        //Pivot.transform.position = transform.position;
+        //Pivot.transform
+        
+    }
+}
